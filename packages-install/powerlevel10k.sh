7a2632bd2a9ae376cd712f31ce6c76f685eb4201
syhner <71605633+syhner@users.noreply.github.com>
2024-04-01 17:16:28 +0200

    fix conditional for installing powerlevel10k

--- packages-install/powerlevel10k.sh
+++ packages-install/powerlevel10k.sh
@@ -3,13 +3,13 @@
 echo "Installing powerlevel10k"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  if [[ -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  if [[ ! -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/powerlevel10k"
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "powerlevel10k"
 elif [[ "$OSTYPE" == "msys" ]]; then
-  if [[ -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  if [[ ! -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/powerlevel10k"
   fi
 fi
