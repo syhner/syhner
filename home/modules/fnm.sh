4cee8c5d5daf0485fbb4f3571f5f1c6f0d4a7a5c
syhner <71605633+syhner@users.noreply.github.com>
2024-04-02 13:28:05 +0200

    add fnm to PATH

--- home/modules/fnm.sh
+++ home/modules/fnm.sh
@@ -1,3 +1,11 @@
+if [ -d "$HOME/.fnm" ]; then
+  export PATH="$HOME/.fnm:$PATH"
+elif [ -n "$XDG_DATA_HOME" ]; then
+  export PATH="$XDG_DATA_HOME/fnm:$PATH"
+else
+  export PATH="$HOME/.local/share/fnm:$PATH"
+fi
+
 eval "$(fnm env --use-on-cd)"
 
 # Completions
