c1a9881c02504d2218ef42eacc31aa15acd471ed
syhner <71605633+syhner@users.noreply.github.com>
2024-04-03 03:30:01 +0200

    setopt.zsh

---
+++ home/source-0/setopt.zsh
@@ -0,0 +1,27 @@
+# https://zsh.sourceforge.io/Doc/Release/Options.html
+
+# 01 - Changing directories
+
+# 02 - Completion
+
+# 03 - Expansion and Globbing
+
+# 04 - History
+setopt extendedHistory
+setopt histIgnoreAllDups
+setopt histNoStore
+setopt incAppendHistoryTime
+
+# 05 - Initialisation
+
+# 06 - Input/Output
+
+# 07 - Job Control
+
+# 08 - Prompting
+
+# 09 - Scripts and Functions
+
+# 10 - Shell emulation
+
+# 11 - Shell state
