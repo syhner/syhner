0669bafb8822c25f606c3bd1964fa204a695a8ed
syhner <71605633+syhner@users.noreply.github.com>
2024-04-03 12:31:52 +0200

    fix git clone for zsh-history-substring-search

--- scripts/install/zsh-history-substring-search.sh
+++ scripts/install/zsh-history-substring-search.sh
@@ -5,8 +5,8 @@ package_name="zsh-history-substring-search"
 echo "Checking for $package_name installation"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  if [[ ! -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/powerlevel10k"
+  if [[ ! -f "$HOME/zsh-history-substring-search/zsh-history-substring-search.zsh" ]]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git "$HOME/zsh-history-substring-search"
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "$package_name"
