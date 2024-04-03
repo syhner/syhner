5e628746c6f3778be7d2847bd5c5564e0c8e3fa3
syhner <71605633+syhner@users.noreply.github.com>
2024-04-03 11:37:08 +0200

    fix zsh-history-substring-search on linux

--- home/source-2/zsh-history-substring-search.zsh
+++ home/source-2/zsh-history-substring-search.zsh
@@ -1,5 +1,5 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  source /usr/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+  source "$HOME/zsh-history-substring-search/zsh-history-substring-search.zsh"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 elif [[ "$OSTYPE" == "msys" ]]; then
--- scripts/install/zsh-history-substring-search.sh
+++ scripts/install/zsh-history-substring-search.sh
@@ -5,7 +5,9 @@ package_name="zsh-history-substring-search"
 echo "Checking for $package_name installation"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  install_package "$package_name"
+  if [[ ! -f "$HOME/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/powerlevel10k"
+  fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "$package_name"
 elif [[ "$OSTYPE" == "msys" ]]; then
