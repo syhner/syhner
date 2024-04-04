7287d2ea2f1a07ede02e10f2bdb221c9d4c73df0
syhner <71605633+syhner@users.noreply.github.com>
2024-04-04 16:44:14 +0200

    fix fzf for linux

--- home/source-1/fzf.sh
+++ home/source-1/fzf.sh
@@ -4,7 +4,15 @@ fi
 
 # Completions
 if [[ $(current_shell) == "bash" ]]; then
-  eval "$(fzf --bash)"
+  if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
+    source_if_exists "$HOME/.fzf.bash"
+  else
+    eval "$(fzf --bash)"
+  fi
 elif [[ $(current_shell) == "zsh" ]]; then
-  eval "$(fzf --zsh)"
+  if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
+    source_if_exists "$HOME/.fzf.zsh"
+  else
+    eval "$(fzf --zsh)"
+  fi
 fi
--- scripts/install/fzf.sh
+++ scripts/install/fzf.sh
@@ -9,7 +9,7 @@ source "$DOTFILES_HOME/source-0/functions.sh"
 package_name="fzf"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  install_package "$package_name"
+  install_package "fzf" "git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "$package_name"
 elif [[ "$OSTYPE" == "msys" ]]; then
