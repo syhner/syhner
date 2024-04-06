310b58d2f0d576bfffcf2ca66abd9129beba4256
syhner <71605633+syhner@users.noreply.github.com>
2024-04-06 17:33:43 +0200

    add ripgrep

--- scripts/install/ripgrep.sh
+++ scripts/install/ripgrep.sh
@@ -5,12 +5,12 @@ export DOTFILES
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)"
 source "$DOTFILES/home/source-0/functions.sh"
 
-package_name="visual-studio-code"
+package_name="ripgrep"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  install_package code
+  install_package "$package_name"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "$package_name"
 elif [[ "$OSTYPE" == "msys" ]]; then
-  install_package Microsoft.VisualStudioCode
+  install_package "BurntSushi.ripgrep.MSVC"
 fi
