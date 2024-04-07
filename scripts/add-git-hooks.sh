80d704ae0d1e5e1dfedcede6e23a21edc61d128d
syhner <71605633+syhner@users.noreply.github.com>
2024-04-08 00:36:16 +0200

    fix git hooks

--- scripts/add-git-hooks.sh
+++ scripts/add-git-hooks.sh
@@ -5,5 +5,5 @@ export DOTFILES
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)"
 source "$DOTFILES/home/source-0/functions.sh"
 
-echo "chmod +x $DOTFILES/scripts/*.sh" >"$DOTFILES/.git/hooks/pre-commit"
+echo "chmod +x $DOTFILES/scripts/**/*.sh" >"$DOTFILES/.git/hooks/pre-commit"
 chmod +x "$DOTFILES/.git/hooks/pre-commit"
