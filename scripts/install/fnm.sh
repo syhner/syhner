79e08f196b0e11599dcd90435ea1f19c9e5ce025
syhner <71605633+syhner@users.noreply.github.com>
2024-04-13 13:40:34 +0200

    install pnpm directly on mac / windows

--- scripts/install/fnm.sh
+++ scripts/install/fnm.sh
@@ -9,11 +9,11 @@ package_name="fnm"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
   install_package "command -v $package_name" "install_package unzip && curl -fsSL https://fnm.vercel.app/install | bash"
+  corepack enable pnpm
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   install_package "$package_name"
-  install_package "corepack"
+  install_package "pnpm"
 elif [[ "$OSTYPE" == "msys" ]]; then
   install_package "Schniz.fnm"
+  install_package "pnpm"
 fi
-
-corepack enable pnpm
