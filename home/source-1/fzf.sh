80eb1c623f1baddb11b32607011b59c7b45ff5cf
syhner <71605633+syhner@users.noreply.github.com>
2024-04-19 19:28:19 +0200

    add fzf previews

--- home/source-1/fzf.sh
+++ home/source-1/fzf.sh
@@ -17,6 +17,13 @@ if ! command -v fzf &>/dev/null; then
   return
 fi
 
+alias fman="compgen -c | fzf | xargs man"
+
+function aliases() {
+  eval "$(alias | fzf | awk -F '=' '{print $1}' | cut -d ' ' -f2)"
+}
+alias a="aliases"
+
 if command -v fd &>/dev/null; then
   export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
   export FZF_CTRL_T_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
@@ -31,9 +38,24 @@ if command -v fd &>/dev/null; then
   }
 fi
 
-alias fman="compgen -c | fzf | xargs man"
+if command -v eza &>/dev/null && command -v bat &>/dev/null && command -v dig &>/dev/null; then
+  _fzf_comprun() {
+    local command="$1"
+    shift
 
-function aliases() {
-  eval "$(alias | fzf | awk -F '=' '{print $1}' | cut -d ' ' -f2)"
-}
-alias a="aliases"
+    case "$command" in
+    cd) fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export | unset) fzf --preview "eval 'echo \${}'" "$@" ;;
+    ssh) fzf --preview 'dig {}' "$@" ;;
+    *) fzf --preview "--preview 'bat -n --color=always --line-range :500 {}'" "$@" ;;
+    esac
+  }
+fi
+
+if command -v bat &>/dev/null; then
+  export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+fi
+
+if command -v eza &>/dev/null; then
+  export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head 200'"
+fi
