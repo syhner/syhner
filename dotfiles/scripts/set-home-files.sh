0d41d98ef2e30a6a3ceef5669857bc176663bab6
syhner <71605633+syhner@users.noreply.github.com>
2024-06-19 00:09:57 +0200

    fix stow gitignore conflicts and set-home-files cleanup

--- dotfiles/scripts/set-home-files.sh
+++ dotfiles/scripts/set-home-files.sh
@@ -1,50 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail # strict mode
 
-# TODO: needs cleaning up
-
 export DOTFILES
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)/dotfiles"
-source "$DOTFILES/home/shell/shell/source/0-functions.sh"
+source "$DOTFILES/home/shell/source/0-functions.sh"
+
+function home_stow() {
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: home_stow <package>"
+    exit 1
+  fi
+
+  local package="$1"
+
+  if [ -f "$package/target.json" ]; then
+    target="$(jq <"$package/target.json" -r ."$(get_os)" | sed "s|~|$HOME|")"
+    # if a target is not speicified for the OS, skip
+    if [[ "$target" == "null" ]]; then
+      return
+    fi
+    mkdir -p "$target"
+    stow --ignore target.json --target="$target" "$package"
+  else
+    stow --target="$HOME" "$package"
+  fi
+}
 
 package=${1:-}
 
 cd "$DOTFILES"/home
 
 if [[ -n "$package" ]]; then
-  echo "Copying files for package $package/home/$package"
   if [[ ! -d "$package" ]]; then
     echo "$DOTFILES/home/shell/$package does not exist"
     exit 1
   fi
 
-  if [ -f "$package/target.json" ]; then
-    target="$(jq <"$package/target.json" -r ."$(get_os)" | sed "s|~|$HOME|")"
-    # if a target is not speicified for the OS, skip
-    if [[ "$target" == "null" ]]; then
-      exit 0
-    fi
-    mkdir -p "$target"
-    stow --ignore target.json --target="$target" "$package"
-  else
-    echo 'hi'
-    stow --target="$HOME" "$package"
-  fi
-
+  home_stow "$package"
   exit 0
 fi
 
 for dir in *; do
-  if [ -f "$dir/target.json" ]; then
-    target="$(jq <"$dir/target.json" -r ."$(get_os)" | sed "s|~|$HOME|")"
-    # if a target is not speicified for the OS, skip
-    if [[ "$target" == "null" ]]; then
-      continue
-    fi
-    mkdir -p "$target"
-    stow --ignore target.json --target="$target" "$dir"
-  else
-    echo 'hi'
-    stow --target="$HOME" "$dir"
-  fi
+  home_stow "$dir"
 done
