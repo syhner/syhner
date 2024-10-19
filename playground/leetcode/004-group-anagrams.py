8a040051c04536e36d2c050de6571f00a187e982
syhner <71605633+syhner@users.noreply.github.com>
2024-10-19 13:33:25 +0100

    leetcode

--- playground/leetcode/004-group-anagrams.py
+++ playground/leetcode/004-group-anagrams.py
@@ -2,13 +2,12 @@
 
 from collections import defaultdict
 
-class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        res = defaultdict(list)
-        
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return res.values()
+def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+    res = defaultdict(list)
+    
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        res[tuple(count)].append(s)
+    return res.values()
---
+++ playground/leetcode/005-top-k-elements-in-list.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/top-k-frequent-elements
+
+def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+	count = {}
+	freq = [[] for _ in range(len(nums) + 1)]
+
+	for num in nums:
+		count[num] = 1 + count.get(num, 0)
+	for num, cnt in count.items():
+		freq[cnt].append(num)
+	
+	res = []
+	for i in range(len(freq) - 1, 0, -1):
+		for num in freq[i]:
+			res.append(num)
+			if len(res) == k:
+				return res
