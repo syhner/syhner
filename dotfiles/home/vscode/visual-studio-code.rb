394bf82ef6721b4f437c55c45e51782ebab93373
syhner <71605633+syhner@users.noreply.github.com>
2024-12-31 13:28:21 +0000

    vscode

---
+++ dotfiles/home/vscode/visual-studio-code.rb
@@ -0,0 +1,42 @@
+cask "visual-studio-code" do
+  arch arm: "darwin-arm64", intel: "darwin"
+
+  version "1.93.1"
+  sha256 arm:   "8bb425d1737c4699db5f2ef2f21c596a41680edf094966ac874d5d67813ab04d",
+         intel: "44fbc8b0c4dd1ad236979dcff08622d3ff09ce21a17f1263bffdc379ae205f97"
+
+  url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
+  name "Microsoft Visual Studio Code"
+  name "VS Code"
+  desc "Open-source code editor"
+  homepage "https://code.visualstudio.com/"
+
+  livecheck do
+    url "https://update.code.visualstudio.com/api/update/#{arch}/stable/latest"
+    strategy :json do |json|
+      json["productVersion"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Visual Studio Code.app"
+  binary "#{appdir}/Visual Studio Code.app/Contents/Resources/app/bin/code"
+
+  uninstall launchctl: "com.microsoft.VSCode.ShipIt",
+            quit:      "com.microsoft.VSCode"
+
+  zap trash: [
+    "~/.vscode",
+    "~/Library/Application Support/Code",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.vscode.sfl*",
+    "~/Library/Caches/com.microsoft.VSCode",
+    "~/Library/Caches/com.microsoft.VSCode.ShipIt",
+    "~/Library/HTTPStorages/com.microsoft.VSCode",
+    "~/Library/Preferences/ByHost/com.microsoft.VSCode.ShipIt.*.plist",
+    "~/Library/Preferences/com.microsoft.VSCode.helper.plist",
+    "~/Library/Preferences/com.microsoft.VSCode.plist",
+    "~/Library/Saved Application State/com.microsoft.VSCode.savedState",
+  ]
+end
--- dotfiles/scripts/install/apps/visual-studio-code.sh
+++ dotfiles/scripts/install/apps/visual-studio-code.sh
@@ -1,46 +1,48 @@
-#!/usr/bin/env bash
-set -euo pipefail # strict mode
+# install v1.93 manually for compat with apc customise ui++
 
-export DOTFILES
-DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)/dotfiles"
-source "$DOTFILES/home/shell/source/0-functions.sh"
+# #!/usr/bin/env bash
+# set -euo pipefail # strict mode
 
-package_name="visual-studio-code"
+# export DOTFILES
+# DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)/dotfiles"
+# source "$DOTFILES/home/shell/source/0-functions.sh"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
-  true # Don't install
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  install_package "$package_name"
-  defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-elif [[ "$OSTYPE" == "msys" ]]; then
-  install_package Microsoft.VisualStudioCode
-fi
+# package_name="visual-studio-code"
 
-if ! command -v code &>/dev/null; then
-  return
-fi
+# if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
+#   true # Don't install
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#   install_package "$package_name"
+#   defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# elif [[ "$OSTYPE" == "msys" ]]; then
+#   install_package Microsoft.VisualStudioCode
+# fi
 
-echo 'Installing Visual Studio Code extensions...'
+# if ! command -v code &>/dev/null; then
+#   return
+# fi
 
-# Used in settings / keybindings
-code --install-extension antfu.where-am-i
-code --install-extension akil-s.tokyo-light
-code --install-extension drcika.apc-extension
-code --install-extension enkia.tokyo-night
-code --install-extension hoovercj.vscode-settings-cycler
-code --install-extension PKief.material-icon-theme
-code --install-extension ryuta46.multi-command
-code --install-extension timonwong.shellcheck
-code --install-extension TomRijndorp.find-it-faster
-code --install-extension usernamehw.errorlens
-code --install-extension vscodevim.vim
+# echo 'Installing Visual Studio Code extensions...'
 
-# Not used in settings / keybindings, improves DX, language agnostic
-code --install-extension aaron-bond.better-comments
-code --install-extension adpyke.codesnap
-code --install-extension beatzoid.http-status-codes
-code --install-extension EditorConfig.EditorConfig
-code --install-extension mikestead.dotenv
-code --install-extension timonwong.shellcheck
+# # Used in settings / keybindings
+# code --install-extension antfu.where-am-i
+# code --install-extension akil-s.tokyo-light
+# code --install-extension drcika.apc-extension
+# code --install-extension enkia.tokyo-night
+# code --install-extension hoovercj.vscode-settings-cycler
+# code --install-extension PKief.material-icon-theme
+# code --install-extension ryuta46.multi-command
+# code --install-extension timonwong.shellcheck
+# code --install-extension TomRijndorp.find-it-faster
+# code --install-extension usernamehw.errorlens
+# code --install-extension vscodevim.vim
 
-echo 'Finished installing Visual Studio Code extensions.'
+# # Not used in settings / keybindings, improves DX, language agnostic
+# code --install-extension aaron-bond.better-comments
+# code --install-extension adpyke.codesnap
+# code --install-extension beatzoid.http-status-codes
+# code --install-extension EditorConfig.EditorConfig
+# code --install-extension mikestead.dotenv
+# code --install-extension timonwong.shellcheck
+
+# echo 'Finished installing Visual Studio Code extensions.'
